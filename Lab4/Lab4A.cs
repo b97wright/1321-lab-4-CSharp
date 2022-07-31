5c98586d1bf0ad5749b48352e84ed2d4651e5c3c
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2022-07-31 08:13:07 -0400

    Lab4a Labb

---
+++ Lab4/Lab4A.cs
@@ -0,0 +1,58 @@
+﻿/*
+    Class: 1321L
+    Section: Section W#1 C#
+    Term: Spring 2022
+    Instructor: Gayathri Yella
+    Name: Brandon Wright
+    Lab#: 4A
+ */
+
+using System;
+
+namespace Lab4
+{
+    class Lab4A
+    {
+        static void Main(string[] args)
+        {
+            double eS = 0;
+            string uI;
+
+            Console.Write("Enter the score of your exam: ");
+            uI = Console.ReadLine();
+            eS = Convert.ToDouble(uI);
+
+            Console.Write("Letter grade is: ");
+
+            if (eS > 97)
+                Console.Write("A+");
+            else if (eS > 94 && eS <= 97)
+                Console.Write("A");
+            else if (eS > 91 && eS <= 94)
+                Console.Write("A-");
+            else if (eS > 88 && eS <= 91)
+                Console.Write("B+");
+            else if (eS > 85 && eS <= 88)
+                Console.Write("B");
+            else if (eS > 82 && eS <= 85)
+                Console.Write("B-");
+            else if (eS > 79 && eS <= 82)
+                Console.Write("C+");
+            else if (eS > 76 && eS <= 79)
+                Console.Write("C");
+            else if (eS > 73 && eS <= 76)
+                Console.Write("C-");
+            else if (eS > 70 && eS <= 73)
+                Console.Write("D+");
+            else if (eS > 67 && eS <= 70)
+                Console.Write("D");
+            else if (eS > 64 && eS <= 67)
+                Console.Write("D-");
+            else
+                Console.Write("F");
+
+
+
+        }
+    }
+}
---
+++ Lab4/Lab4B.cs
@@ -0,0 +1,110 @@
+﻿/*
+    Class: 1321L
+    Section: Section W#1 C#
+    Term: Spring 2022
+    Instructor: Gayathri Yella
+    Name: Brandon Wright
+    Assignment#: Assignment3B
+*/
+
+using System;
+
+namespace Assignment3
+{
+    class Assignment3B
+    {
+        static void Main(string[] args)
+        {
+            string uI;
+            char weekDay, time;
+
+
+            Console.WriteLine("[Instructor Scheduler]");
+            Console.Write("What day of the week is it? ");
+            uI = Console.ReadLine();
+            weekDay = Convert.ToChar(uI);
+
+            Console.Write("What time of day? ");
+            uI = Console.ReadLine();
+            time = Convert.ToChar(uI);
+
+            switch (weekDay)
+            {
+                case 'M':
+                    if (time == 'a')
+                    {
+                        Console.WriteLine("On Monday Morning, you’ll be teaching C++, section E04!");
+                        break;
+                    }
+                    else if (time == 'p')
+                    {
+                        Console.WriteLine("On Monday Evening, you’ll be teaching Java, section J02!");
+                        break;
+                    }
+                    Console.WriteLine("That’s not a valid day or time!!");
+                    break;
+
+                case 'T':
+                    if (time == 'a')
+                    {
+                        Console.WriteLine("On Tuesday Morning, you’ll be holding office hours!");
+                        break;
+                    }
+                    else if (time == 'p')
+                    {
+                        Console.WriteLine("On Tuesday Evening, you’ll be teaching C#, section W#1!");
+                        break;
+                    }
+                    Console.WriteLine("That’s not a valid day or time!!");
+                    break;
+
+                case 'W':
+                    if (time == 'a')
+                    {
+                        Console.WriteLine("On Wednesday Evening, you’ll be teaching C#, section #02!");
+                        break;
+                    }
+                    else if (time == 'p')
+                    {
+                        Console.WriteLine("On Wednesday Afternoon, you’ll have some free time!");
+                        break;
+                    }
+                    Console.WriteLine("That’s not a valid day or time!!");
+                    break;
+
+                case 'R':
+                    if (time == 'a')
+                    {
+                        Console.WriteLine("On Thursday Morning, you’ll be holding office hours!");
+                        break;
+                    }
+                    else if (time == 'p')
+                    {
+                        Console.WriteLine("On Thursday Evening, you’ll be teaching C++, section WE3!");
+                        break;
+                    }
+                    Console.WriteLine("That’s not a valid day or time!!");
+                    break;
+
+                case 'F':
+                    if (time == 'a')
+                    {
+                        Console.WriteLine("On Friday Morning, you’ll be teaching Java, section WJ3!");
+                        break;
+                    }
+                    else if (time == 'p')
+                    {
+                        Console.WriteLine("On Friday Evening, you’ll have some free time!");
+                        break;
+                    }
+                    Console.WriteLine("That’s not a valid day or time!!");
+                    break;
+                default:
+                    Console.WriteLine("That’s not a valid day or time!!");
+                    break;
+
+            }
+
+        }
+    }
+}
